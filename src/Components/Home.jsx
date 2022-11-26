e9a3503f74f19d9cb34fe672c6e59f1e5058f339
Decxo360 <diegolundstedt99@gmail.com>
2022-11-25 21:42:25 -0300

    Vista de componentes como el navbar y el producto

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Navbar } from './Navbar'
+import { ProductComponent } from './ProductComponent'
 
 
 export const Home = () => {
   return (
-    <div className='flex flex-col justify-start items-center'>
+    <div className='flex flex-col w-[100%]'>
+      <Navbar/>
+      <div className='w-[100%] flex flex-row justify-between'>
+        <div className='w-[33%]'></div>
+        <ProductComponent/>
+        <ProductComponent/>
+        <div className='w-[33%]'></div>
+      </div>
     </div>
   )
 }
